5e1c09bd7a37515626178a2089845db833a48472
Marcos Gómez <111522224+Marcos7gomez@users.noreply.github.com>
2024-02-10 10:50:56 -0300

    Update 01-variables.js

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -11,7 +11,7 @@ apellido = 'Gomez';
 
 // UpperCamelCase: el inicio de cada palabra debe ser en mayus
 // camelCase: cada palabra debe ir en mayus menos la primera
-// sanke_case: cada palabra debe estar separada por un guion bajo
+// snake_case: cada palabra debe estar separada por un guion bajo
 
 // Tipos de datos
 // Primitivos: Number, String, Boolean, Undefined, Null
